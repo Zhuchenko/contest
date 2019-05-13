3cf8014171419fc4ced364c6e54be1873dcc9b95
Kristina Volyakova <Kristina.Volyakova@confirmit.com>
2019-05-13 14:04:33 +0300

    Removed unused packages

--- server/mongoose/index.js
+++ server/mongoose/index.js
@@ -1,16 +1,10 @@
 import mongoose from 'mongoose'
-import passportLocalMongoose from 'passport-local-mongoose'
-import serverConfig from '../development.server.config.js'
-import {UserSchema} from "./schemas";
+import {serverConfig} from '../config/serverConfig'
 
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.Promise = Promise;
 
-UserSchema.plugin(passportLocalMongoose);
-
 export const connect = () => {
     return mongoose.connect(serverConfig.databaseConnectionURL, { useNewUrlParser: true })
 };
-
-export const maxAllowedAttachmentLength = 16 * 1024 * 1024; // 16MB
