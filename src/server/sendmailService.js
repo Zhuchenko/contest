a88bb462950bceaeaa8074441f99fc5cc5214c64
Kristina <kristya-french@mail.ru>
2020-01-18 18:34:12 +0300

    Update sendmailService.js

--- src/server/sendmailService.js
+++ src/server/sendmailService.js
@@ -1,8 +1,5 @@
-//import nodemailer from 'nodemailer'
 const sendmail = require('sendmail')({
-    // devPort: 25, // Default: False
-    // devHost: 'localhost', // Default: localhost
-    smtpPort: 25, // Default: 25
+    smtpPort: 25,
     smtpHost: 'localhost'
 });
 
@@ -15,44 +12,6 @@ export const VerifyEmail = (email, code) => {
         to: email,
         subject: 'VerifyEmail',
         html: verifyEmailUrl + code,
-    }, function(err, reply) {
-        console.log(err && err.stack);
-        console.dir(reply);
-    });
-};
-
-// export const VerifyEmail = async (email, code) => {
-//     let testAccount = await nodemailer.createTestAccount();
-//
-//     // create reusable transporter object using the default SMTP transport
-//     let transporter = nodemailer.createTransport({
-//         host: 'localhost',
-//         port: 587,
-//         secure: false, // true for 465, false for other ports
-//         auth: {
-//            user: testAccount.user, // generated ethereal user
-//             pass: testAccount.pass // generated ethereal password
-//         }
-//     });
-//
-//     // send mail with defined transport object
-//     let info = await transporter.sendMail({
-//         from, // sender address
-//         to: email, // list of receivers
-//         subject: 'Verify Email', // Subject line
-//         text: 'Hello world?', // plain text body
-//         html: verifyEmailUrl + '?code=' + code // html body
-//     });
-//
-//     console.log('Message sent: %s', info.messageId);
-// };
-
-export const ChangePassword = (email, code) => {
-    sendmail({
-        from,
-        to: email,
-        subject: 'ChangePassword',
-        html: code,
     }, function(err, reply) {
         console.log(err && err.stack);
         console.dir(reply);
